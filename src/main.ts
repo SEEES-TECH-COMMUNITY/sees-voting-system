212ff45c3a9283a47ecdb8506604def388874eb1
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-22 22:57:34 +0100

    chore: added lovcla hosyt

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     defaultVersion: '1',
   });
   app.enableCors({
-    origin: [ENV.DASHBOARD_URL],
+    origin: [ENV.DASHBOARD_URL, 'http://localhost:3000'],
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'PATCH'],
     credentials: true,
   });
