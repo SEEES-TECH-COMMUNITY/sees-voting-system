7110d1516e9958546d52a90a330807312ed08104
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-25 00:24:00 +0100

    chore: commented lopgic for creating candidates

--- src/candidate/candidate.service.ts
+++ src/candidate/candidate.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Position } from 'src/shared/constants/positions';
 import { Candidate, CandidateDocument } from 'src/shared/db/candidate.schema';
 import { CreateCandidateDto } from 'src/shared/dto/creat.dto';
 
@@ -16,7 +17,12 @@ export class CandidateService {
     const newCandidate = new this.candidateModel(candidate);
     return newCandidate.save();
   }
-
+  async createCandidateBulk(candidates: Array<CreateCandidateDto>) {
+    for (const candidate of candidates) {
+      await this.checkCandidateExists(candidate.full_name, candidate.level);
+    }
+    return this.candidateModel.insertMany(candidates);
+  }
   async checkCandidateExists(full_name: string, level: string) {
     const candidate = await this.candidateModel.findOne({
       full_name,
@@ -37,7 +43,28 @@ export class CandidateService {
     return this.candidateModel.findByIdAndDelete(candidate_id);
   }
 
-  getAllCandidates() {
-    return this.candidateModel.find();
+  async getAllCandidates() {
+    const positionOrder = Object.values(Position);
+    const candidates = await this.candidateModel.aggregate([
+      {
+        $addFields: {
+          positionRank: {
+            $indexOfArray: [positionOrder, '$position'],
+          },
+        },
+      },
+      {
+        $sort: { positionRank: 1 },
+      },
+      {
+        $project: { positionRank: 0 }, // Optionally remove the rank field from the output
+      },
+    ]).exec();
+    return candidates.map((candidate) => {
+      candidate.id = candidate._id;
+      delete candidate._id;
+      delete candidate.__v;
+      return candidate;
+    });
   }
 }
