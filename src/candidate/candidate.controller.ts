81a4d01ae03488f9d4379c56c3a25c1748c539f0
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-25 00:24:22 +0100

    chore: commneted logic for creating participant

--- src/candidate/candidate.controller.ts
+++ src/candidate/candidate.controller.ts
@@ -1,15 +1,20 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CandidateService } from './candidate.service';
-import { CreateCandidateDto } from '../shared/dto/creat.dto';
+import { CreateCandidateBulkDto, CreateCandidateDto } from '../shared/dto/creat.dto';
 
 @Controller('candidate')
 export class CandidateController {
   constructor(private service: CandidateService) {}
 
-  @Post('create')
-  async createCandidate(@Body() payload: CreateCandidateDto) {
-    return await this.service.createCandidate(payload);
-  }
+  // @Post('create')
+  // async createCandidate(@Body() payload: CreateCandidateDto) {
+  //   return await this.service.createCandidate(payload);
+  // }
+
+  // @Post('create/bulk')
+  // async createCandidateBulk(@Body() payload: CreateCandidateBulkDto) {
+  //   return await this.service.createCandidateBulk(payload.candidates);
+  // }
 
   @Get('all')
   getCandidates() {
