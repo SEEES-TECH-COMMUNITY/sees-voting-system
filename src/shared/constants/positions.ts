f1a47093bd56cee4b907ea15b3cab45b19e4f93f
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2024-10-25 00:24:31 +0100

    feat: added ne wpermissions

--- src/shared/constants/positions.ts
+++ src/shared/constants/positions.ts
@@ -1,4 +1,12 @@
 export enum Position {
     PRESIDENT = 'president',
     VICE_PRESIDENT = 'vice_president',
+    SECRETARY_GENERAL = 'secretary_general',
+    ASSISTANT_SECRETARY_GENERAL = 'assistant_secretary_general',
+    TREASURER = 'treasurer',
+    FINANCIAL_SECRETARY = 'financial_secretary',
+    PUBLIC_RELATIONS_OFFICER = 'public_relations_officer',
+    DIRECTOR_OF_WELFARE = 'director_of_welfare',
+    DIRECTOR_OF_SPORTS = 'director_of_sports',
+    DIRECTOR_OF_SOCIALS = 'director_of_socials',
 }
