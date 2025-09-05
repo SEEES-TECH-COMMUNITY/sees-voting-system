02e52ae6f3308dc69b0eca233e2ea03aafe0e0fb
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-05 23:42:27 +0100

    feat: add FingerPrint schema for student fingerprint data

---
+++ src/shared/db/fingerprint.schema.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type FingerPrintDocument = HydratedDocument<FingerPrint>;
+
+@Schema()
+export class FingerPrint {
+  id: string;
+
+  @Prop({
+    type: mongoose.Schema.ObjectId,
+    required: true,
+  })
+  student_id: string;
+
+  @Prop()
+  fingerPrint: string;
+}
+export const FingerPrintSchema = SchemaFactory.createForClass(FingerPrint);
+
+FingerPrintSchema.virtual('id').get(function () {
+  return this._id.toHexString();
+});
+
+FingerPrintSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
