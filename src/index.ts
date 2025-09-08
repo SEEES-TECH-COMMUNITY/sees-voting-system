9a5e4a7fc9197adaa410cf65d453a98f016bb338
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-08 22:58:52 +0100

    feat: integrate Treblle for enhanced API monitoring and add global validation pipes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import './config/validateEnv';
 import { NestFactory } from '@nestjs/core';
+const treblle = require('@treblle/express');
+
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 import { ENV } from './config/env';
 import { ForbiddenExceptionFilter } from './shared/filters/forbidden.exception-filter';
+import { NextFunction, Request, Response } from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
@@ -18,6 +22,26 @@ async function bootstrap() {
     credentials: true,
   });
   app.useGlobalFilters(new ForbiddenExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      always: true,
+    }),
+  );
+  if (ENV.NODE_ENV !== 'development') {
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      if (new RegExp('swagger').test(req.path)) {
+        return next();
+      }
+      return treblle({
+        apiKey: process.env.TREBLLE_API_KEY,
+        projectId: process.env.TREBLLE_PROJECT_ID,
+        additionalFieldsToMask: [],
+        blacklistPaths: [],
+      })(req, res, next);
+    });
+  }
   app.use(
     session({
       secret: 'vgz0WHhVlip359/LZmFVZZGVhXAceaJ1vRBhBxlpQ/w=',
