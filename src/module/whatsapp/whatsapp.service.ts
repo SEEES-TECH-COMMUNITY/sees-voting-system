8b394308b8830b5b7546911e88909570b6668b90
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-09 14:02:14 +0100

    fix(whatsapp): update message template to include voting link for user guidance

--- src/module/whatsapp/whatsapp.service.ts
+++ src/module/whatsapp/whatsapp.service.ts
@@ -11,7 +11,7 @@ export class WhatsappService {
     mat_number: string,
     password: string,
   ) {
-    const text = `Welcome to SEES Voting System. Your login details are as follows:\nMatric Number: ${mat_number}\nPassword: ${password}\nPlease keep this information confidential and do not share it with anyone.\nThank you for being a part of our community!`;
+    const text = `Welcome to SEES Voting System. Your login details are as follows:\nMatric Number: ${mat_number}\nPassword: ${password}\nPlease keep this information confidential and do not share it with anyone.\n You can proceed to vote on https://voting.seees-uniben.org .\nThank you for being a part of our community!`;
     const url = `${this.BASE_WHATSAPP_URL}/api/sendText`;
     const payload = {
       chatId: `${phone_number.replace('+', '')}@c.us`,
