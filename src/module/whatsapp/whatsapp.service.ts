71009f4120e4ef38ed756638b3a2467c11c7242a
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-18 18:03:59 +0100

    fix(whatsapp): implement random delays for sending messages to enhance user experience

--- src/module/whatsapp/whatsapp.service.ts
+++ src/module/whatsapp/whatsapp.service.ts
@@ -11,12 +11,14 @@ export class WhatsappService {
     mat_number: string,
     password: string,
   ) {
+    const firstDelay = Math.random() * (10000 - 5000) + 5000; // Random delay between 5 to 10 seconds
+    const secondsDelay = Math.random() * (15000 - 5000) + 5000; // Random delay between 5 to 10 seconds
     await this.startTyping(phone_number);
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await new Promise((resolve) => setTimeout(resolve, firstDelay));
     const text = `Welcome to the SEES Voting System. \nYour login details will be sent to this number at the start of the election tomorrow. \nThank you for being part of our community!`;
     const url = `${this.BASE_WHATSAPP_URL}/api/sendText`;
     await this.stopTyping(phone_number);
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await new Promise((resolve) => setTimeout(resolve, secondsDelay));
     const payload = {
       chatId: `${phone_number.replace('+', '')}@c.us`,
       text,
