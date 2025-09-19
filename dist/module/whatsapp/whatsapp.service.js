5d0cd51ce551c620f6ae88d734e30e556c936125
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-19 20:41:09 +0100

    Refactor code structure for improved readability and maintainability

--- dist/module/whatsapp/whatsapp.service.js
+++ dist/module/whatsapp/whatsapp.service.js
@@ -105,6 +105,8 @@ let WhatsappService = class WhatsappService {
             .findOne({ phone_number: from.replace('@c.us', '') })
             .select('+password')
             .exec();
+        await this.messagePhone(from.replace('@c.us', ''), 'Voting concluded. Thank you for your interest.');
+        return 'OK';
         if (!student) {
             console.log('Phone number not registered:', from);
             return;
