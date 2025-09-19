54d7f358a3f3391a7ca4fd86e6ccb45450024c3b
Onyela Udochukwuka <udochukwukaonyela@gmail.com>
2025-09-19 15:57:35 +0100

    feat(app): add BullMQ integration for job processing

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import configuration from './config/configuration';
 import { WhatsappModule } from './module/whatsapp/whatsapp.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   controllers: [AppController],
@@ -28,6 +29,14 @@ import { APP_GUARD } from '@nestjs/core';
     MongooseModule.forRoot(ENV.DATABASE_URL, { dbName: 'voting-service' }),
     AuthModule,
     StudentsModule,
+    BullModule.forRoot({
+      connection: {
+        host: ENV.REDIS_HOST,
+        port: 14947,
+        username: ENV.REDIS_USERNAME,
+        password: ENV.REDIS_PASSWORD,
+      },
+    }),
   ],
   providers: [
     {
